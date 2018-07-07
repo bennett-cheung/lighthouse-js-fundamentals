9e167b4514e309cf1c9a1279dcdb8379443a5546
Bennett Cheung <bennett_cheung@hotmail.com>
2018-07-07 05:32:18 +0000

    fixed formatting

--- loopy.js
+++ loopy.js
@@ -1,27 +1,25 @@
 function loopy(){
 
-for (var i = 100; i <= 200; i++) {
-  var printValue = "";
+  for (var i = 100; i <= 200; i++) {
+    var printValue = "";
+    if (i % 3 === 0 && i % 4 === 0)
+    {
+      printValue = "LoopyLighthouse";
+    }
+    else if (i % 3 === 0)
+    {
+      printValue = "Loopy";
+    }
+    else if (i % 4 === 0)
+    {
+      printValue = "Lighthouse";
+    }
+    else
+    {
+      printValue = i;
+    }
 
-
-  if (i % 3 === 0 && i % 4 === 0)
-  {
-    printValue = "LoopyLighthouse";
-  }
-  else if (i % 3 === 0)
-  {
-    printValue = "Loopy";
-  }
-  else if (i % 4 === 0)
-  {
-    printValue = "Lighthouse";
-  }
-  else
-  {
-    printValue = i;
-  }
-
-  console.log(printValue);
+    console.log(printValue);
   }
 }
 
