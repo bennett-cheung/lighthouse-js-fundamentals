a12a02b5935ed8765bc85dd6428a00febb5ea206
Bennett Cheung <bennett_cheung@hotmail.com>
2018-07-07 05:30:41 +0000

    added loopy

---
+++ loopy.js
@@ -0,0 +1,28 @@
+function loopy(){
+
+for (var i = 100; i <= 200; i++) {
+  var printValue = "";
+
+
+  if (i % 3 === 0 && i % 4 === 0)
+  {
+    printValue = "LoopyLighthouse";
+  }
+  else if (i % 3 === 0)
+  {
+    printValue = "Loopy";
+  }
+  else if (i % 4 === 0)
+  {
+    printValue = "Lighthouse";
+  }
+  else
+  {
+    printValue = i;
+  }
+
+  console.log(printValue);
+  }
+}
+
+loopy();
